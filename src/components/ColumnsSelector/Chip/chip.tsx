a57aab6ec248bc7e67874d14acfdfddb86d1be2d
Sneyder <sneyder2328@gmail.com>
2020-08-17 17:38:49 -0400

    Add redux store
    Toggle chips on click

--- src/components/ColumnsSelector/Chip/chip.tsx
+++ src/components/ColumnsSelector/Chip/chip.tsx
@@ -1,24 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import "./chip.scss";
+import {createAction} from "@reduxjs/toolkit";
+import {useDispatch} from "react-redux";
+
+export const toggleColumnActive = createAction<number>('TOGGLE_COLUMN_ACTIVE')
 
 type Props = {
+    index: number;
     label: string;
     isActive: boolean;
     isEditable: boolean;
-
 }
-
-export const Chip: React.FC<Props> = (props) => {
-    const [isActive, setActive] = useState(props.isActive)
+export const Chip: React.FC<Props> = ({isActive, isEditable, label, index}) => {
+    const dispatch = useDispatch()
 
     const handleClick = () => {
-
+        isEditable && dispatch(toggleColumnActive(index))
     };
 
     return (
-        <div className={classnames('chip', {'active': isActive, 'editable': props.isEditable})} onClick={handleClick}>
-            <span>{props.label}</span>
+        <div className={classnames('chip', {'active': isActive, 'editable': isEditable})} onClick={handleClick}>
+            <span>{label}</span>
         </div>
     )
 }
--- src/components/ColumnsSelector/columnsSelector.tsx
+++ src/components/ColumnsSelector/columnsSelector.tsx
@@ -1,49 +1,17 @@
 import React from 'react';
 import {Chip} from "./Chip/chip";
+import { useSelector } from 'react-redux'
 import "./columnsSelector.scss"
 
-const tableColumns = [
-    {
-        label: "fi",
-        activeByDefault: true
-    },
-    {
-        label: "fri",
-        activeByDefault: true
-    },
-    {
-        label: "Fi",
-        activeByDefault: true
-    },
-    {
-        label: "Fri",
-        activeByDefault: true
-    },
-    {
-        label: "mi",
-        activeByDefault: true
-    },
-    {
-        label: "mi*fi",
-        activeByDefault: true
-    },
-    {
-        label: "mi-x̄",
-        activeByDefault: true
-    },
-    {
-        label: "(mi-x̄)^2",
-        activeByDefault: false
-    },
-    {
-        label: "(mi-x̄)^2*fi",
-        activeByDefault: true
-    }
-];
+export const ColumnsSelector = () => {
 
-export const ColumnsSelector  = () => (
-    <div className={'columns-selector container'}>
-        <Chip isEditable={false} label={'Categoria'} isActive={true}/>
-        {tableColumns.map((column, index) => (<Chip isEditable={true} isActive={column.activeByDefault} label={column.label} key={index}/>))}
-    </div>
-);
+    // @ts-ignore
+    const columns = useSelector(state => state.columnsTable)
+    console.log('columnsSelector=', columns);
+    return (
+        <div className={'columns-selector container'}>
+            {columns.map((column: {label:string;active: boolean}, index: number) => (
+                <Chip index={index} isEditable={index!==0} isActive={column.active} label={column.label} key={index}/>))}
+        </div>
+    )
+};
