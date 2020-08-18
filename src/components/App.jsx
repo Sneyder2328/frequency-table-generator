12ecf604699216c07ed552687e1c83952454e5c9
Kevin <chengkev2000@gmail.com>
2020-08-18 08:53:43 -0400

    added zingchart and progress on graph components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import {AppHeader} from "./AppHeader/appHeader";
-import {DataEntry} from "./DataEntry/dataEntry";
-
+import React from "react";
+import { AppHeader } from "./AppHeader/appHeader";
+import { DataEntry } from "./DataEntry/dataEntry";
+import Histogram from "./Graphs/Histogram/Histogram.jsx";
+import Pie from "./Graphs/Sectors/Pie.jsx";
 
 function App() {
   return (
     <div className="App">
-        <AppHeader/>
-        <DataEntry/>
+      <AppHeader />
+      <DataEntry />
     </div>
   );
 }
---
+++ src/components/Graphs/Histogram/readme.md
@@ -0,0 +1,13 @@
+HOW TO USE COMPONENT HISTOGRAM
+
+ej:
+fi of every interval of the table
+series=[{ values: [1, 2, 3] }]
+
+for the labels in each interval in the x-axis
+labels=[1,2,3,4]
+
+title= the title of the graph
+text= the text of the graph
+scaleXName= the label displayed along the x Axis
+scaleYName= the label displayed along the y Axis
