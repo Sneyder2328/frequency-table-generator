f572b3344037f9dc00ff512cc0c4cb7c8e5ae58f
Sneyder <sneyder2328@gmail.com>
2020-08-18 22:50:47 -0400

    Try fix netlify issue

--- src/components/UI/Chip/chip.tsx
+++ src/components/UI/Chip/chip.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
 import "./chip.scss";
-import {createAction} from "@reduxjs/toolkit";
-import {useDispatch} from "react-redux";
 
 type Props = {
     label: string;
