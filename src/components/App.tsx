a43c5e9888673ebef43c39ba95b85e75a7072859
Kevin <chengkev2000@gmail.com>
2020-08-19 13:58:45 -0400

    versions issue

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,45 +1,26 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {AppHeader} from "./AppHeader/appHeader";
-import {DataEntry} from "./DataEntry/dataEntry";
-import {ColumnsSelector} from "./ColumnsSelector/columnsSelector";
-import {store} from "../store";
-import {Legend} from "./Legend/legend";
-import {DataSet} from "./DataSet/dataSet";
-import {GraphicsSelector} from "./GraphicsSelector/graphicsSelector";
-import {FrequencyTable} from "./FrequencyTable/frequencyTable";
-import {DataSummary} from "./DataSummary/dataSummary";
-import {Histogram} from "./Graphs/Histogram/Histogram"
+import React from "react";
+import { Provider } from "react-redux";
+import { AppHeader } from "./AppHeader/appHeader";
+import { DataEntry } from "./DataEntry/dataEntry";
+import { ColumnsSelector } from "./ColumnsSelector/columnsSelector";
+import { store } from "../store";
+import { Legend } from "./Legend/legend";
+import { DataSet } from "./DataSet/dataSet";
+import { GraphicsSelector } from "./GraphicsSelector/graphicsSelector";
+import Actions from "./Actions/Actions";
 
 export const App = () => {
-    return (
-        <Provider store={store}>
-            <div className="App">
-                <AppHeader/>
-                <DataEntry/>
-                <ColumnsSelector/>
-                <FrequencyTable/>
-                <DataSummary/>
-                <Legend/>
-                <DataSet/>
-                <GraphicsSelector/>
-                <Histogram
-                    title={'Histograma'}
-                    scaleXName={"nombre var"}
-                    scaleYName={"fi"}
-                    text={"subtitulo"}
-                    hasLines={false}
-                />
-                <Histogram
-                    title={'Columnas con lineas'}
-                    scaleXName={"nombre var"}
-                    scaleYName={"fi"}
-                    text={"subtitulo"}
-                    hasLines={true}
-                />
-
-
-            </div>
-        </Provider>
-    );
-}
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppHeader />
+        <DataEntry />
+        <ColumnsSelector />
+        <Legend />
+        <DataSet />
+        <Actions />
+        <GraphicsSelector />
+      </div>
+    </Provider>
+  );
+};
